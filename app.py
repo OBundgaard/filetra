e0ff1e242d873dd5e6c9b7992dd39753e97a235f
Oliver <oliverbundgaard@live.dk>
2024-05-14 11:51:17 +0200

    Upload and download is now working for the basics

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask, request, render_template, send_from_directory
+import os
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def main():
+    files = os.listdir('./files')
+    filenames = [f for f in files]
+
+    return render_template("index.html", files=filenames)
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(f'./files/{f.filename}')
+        return render_template("upload.html")
+
+
+@app.route('/download', methods=['GET'])
+def download_file():
+    if request.method == 'GET':
+        args = request.args
+        return send_from_directory('./files', args['filename'], as_attachment=True)
 
 
 if __name__ == '__main__':

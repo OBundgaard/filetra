53b76cedf2a01a1f9c100e660527dccf93130c95
Oliver <oliverbundgaard@live.dk>
2024-05-16 12:59:18 +0200

    Added basic restricted access to view, download, and upload

--- app.py
+++ app.py
@@ -1,22 +1,58 @@
+import json
+
 from flask import Flask, request, render_template, send_from_directory
 import os
 
 app = Flask(__name__)
 
 
+def get_allowed_groups():
+    with open('./data/user.json', 'r') as file:
+        numbers = json.load(file)
+    return [str(num) for num in numbers]
+
+
 @app.route('/')
 def main():
-    files = os.listdir('./files')
-    filenames = [f for f in files]
+    path = f'./files'
+    elements = os.listdir(path)
 
-    return render_template("index.html", files=filenames)
+    allowed_groups = get_allowed_groups()
+    groups = [element for element in elements if not os.path.isfile(os.path.join(path, element)) and element in allowed_groups]
+
+    return render_template("index.html", groups=groups)
+
+
+@app.route('/view', methods=['GET', 'POST'])
+def view_files():
+    args = request.args
+
+    path = f'./files/{args["group_id"]}'
+    files = os.listdir(path)
+    filenames = [f for f in files if os.path.isfile(os.path.join(path, f))]
+
+    return render_template("view.html", files=filenames, group_id=args["group_id"], allowed=(args["group_id"] in get_allowed_groups()))
 
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
-        f = request.files['file']
-        f.save(f'./files/{f.filename}')
+        args = request.args
+
+        file = request.files['file']
+        file_path = f'./files/{args['group_id']}/{file.filename}'
+
+        # If the file exists, add the count to the name: e.g. "cat (1).jpg"
+        if os.path.isfile(file_path):
+            base, extension = os.path.splitext(file.filename)
+            counter = 1
+            new_file_path = f'./files/{args['group_id']}/{base} ({counter}){extension}'
+            while os.path.isfile(new_file_path):
+                counter += 1
+                new_file_path = f'./files/{args['group_id']}/{base} ({counter}){extension}'
+            file_path = new_file_path
+
+        file.save(file_path)
         return render_template("upload.html")
 
 
@@ -24,7 +60,7 @@ def upload_file():
 def download_file():
     if request.method == 'GET':
         args = request.args
-        return send_from_directory('./files', args['filename'], as_attachment=True)
+        return send_from_directory(f'./files/{args['group_id']}', args['filename'], as_attachment=True)
 
 
 if __name__ == '__main__':

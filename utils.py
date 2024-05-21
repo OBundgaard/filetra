5705435cbab98b90c67e62e3cd34560ea2e31764
Oliver <oliverbundgaard@live.dk>
2024-05-21 10:37:41 +0200

    Added docs to the uuid generation method

--- utils.py
+++ utils.py
@@ -1,7 +1,13 @@
 import uuid
 
 
-def generate_uuid():
-    return uuid.uuid4()
+def generate_uuid() -> str:
+    """A method to generate a unique UUID
 
+    Returns
+    -------
+    str
+        The unique UUID
+    """
 
+    return str(uuid.uuid4())
